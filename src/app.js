01eb9aa6adda2761c1cf54b5cc7e487bdec7cace
staraspirant <nehaninawe02@gmail.com>
2026-09-09 18:00:42 +0530

    made 2 apis one is get user and fetch all user

--- src/app.js
+++ src/app.js
@@ -15,6 +15,28 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
 connectDB()
   .then(() => {
     console.log("database connection established...");
